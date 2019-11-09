b83a53ceb3baf5b9eaef744fba53bd2b66447a71
Nico Einsidler <nicoeinsidler@gmail.com>
2019-11-09 22:53:27 +0100

    added comments

--- module/sub_b/b.py
+++ module/sub_b/b.py
@@ -1,25 +1,21 @@
-# 1) attempt
+# 1st attempt
 # from ..sub_sub_a import a
 # print(a.test_function())
 
-# 2) attempt
+# 2nd attempt
 # import importlib
 # importlib.import_module('..sub_sub_a', 'module')
 # print(sub_sub_a.test_function())
 
 
-# 3) attempt
+# 3rd attempt
 import sys
 import pathlib
+
 PATH_TO_MODULE = pathlib.Path('../..').resolve().absolute()
-print(PATH_TO_MODULE)
 if str(PATH_TO_MODULE) not in sys.path:
     sys.path.insert(0, str(PATH_TO_MODULE))
 
-print(sys.path)
-
 import module.sub_a.sub_sub_a as a
-
-print(a.__dir__)
 
 print(a.test_function())
